d8ba6e85c0b0b6cfb042e38801a01a91a78fa102
jinhwansuh <nick02190219@gmail.com>
2024-11-09 03:51:19 +0900

    design: 최대 max-width 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import '~/styles/global/global.css';
+import * as S from './layout.css';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -26,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <div className={S.layout}>{children}</div>
       </body>
     </html>
   );
