570772422c2388959081d500a7736031a4be1ab5
jinhwansuh <nick02190219@gmail.com>
2024-11-12 17:59:17 +0900

    feat: searchInput 구현

---
+++ src/components/Main/SearchInput/SearchInput.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Button, Input } from '~/components/atomic';
+import * as S from './SearchInput.css';
+
+interface SearchInputProps {
+  defaultValue?: string;
+}
+
+export default function SearchInput({ defaultValue }: SearchInputProps) {
+  const router = useRouter();
+  const [value, setValue] = useState('');
+
+  const handleSearchClick = () => {
+    if (!value) return;
+
+    router.push(`/search?query=${value}`);
+  };
+
+  return (
+    <div className={S.InputWrapper}>
+      <Input
+        data-cy='search-input'
+        onChange={(e) => setValue(e.target.value)}
+        defaultValue={defaultValue}
+      />
+      <Button onClick={handleSearchClick}>Search</Button>
+    </div>
+  );
+}
