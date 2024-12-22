f219f608512225a17bfcdc61576c6e2cfebe6018
jinhwansuh <nick02190219@gmail.com>
2024-12-23 02:04:09 +0900

    fix: search params 에러 수정

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
-export default function SearchPage() {
+function Search() {
   const searchParams = useSearchParams();
   const query = searchParams.get('query');
 
@@ -10,5 +11,13 @@ export default function SearchPage() {
     <div>
       <div>search: {query}</div>
     </div>
+  );
+}
+
+export default function SearchPage() {
+  return (
+    <Suspense fallback={<div>loading...</div>}>
+      <Search></Search>
+    </Suspense>
   );
 }
