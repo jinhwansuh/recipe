f941553c16e9f2f867a000bce9a3fee7c2db252a
jinhwansuh <nick02190219@gmail.com>
2025-01-04 20:35:41 +0900

    config: type any rule 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,6 +26,7 @@ const eslintConfig = [
       'prettier/prettier': 'error',
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
     },
   },
   eslintConfigPrettier,
