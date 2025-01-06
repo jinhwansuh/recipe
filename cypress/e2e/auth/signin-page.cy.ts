b6add4f1661e918e1db93b644334ecefb9ebc54e
jinhwansuh <nick02190219@gmail.com>
2025-01-07 01:08:15 +0900

    test: login test 추가

--- cypress/e2e/auth/signin-page.cy.ts
+++ cypress/e2e/auth/signin-page.cy.ts
@@ -10,4 +10,15 @@ describe('로그인 페이지', () => {
     cy.url().should('eq', Cypress.config().baseUrl + '/');
     cy.getCookie('authjs.session-token').should('exist');
   });
+
+  it('잘못된 아이디로 로그인하면 toast 메세지가 나타난다.', () => {
+    cy.visit('/signin');
+    cy.getByCy('email-input').type('test123123123123@test.com');
+    cy.getByCy('password-input').type(`${'123123'}{enter}`, { log: false });
+
+    cy.intercept('GET', 'api/auth/session').as('session');
+    cy.wait('@session');
+
+    cy.getByCy('toast').should('be.visible');
+  });
 });
