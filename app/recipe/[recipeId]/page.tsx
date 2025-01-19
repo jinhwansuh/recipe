975c7a9edc4f24f1a8160c08dd8e365e0e23c0df
jinhwansuh <nick02190219@gmail.com>
2025-01-19 17:43:36 +0900

    feat: recipe page 구현

---
+++ app/recipe/[recipeId]/page.tsx
@@ -0,0 +1,95 @@
+import { getRecipe } from '~/lib/actions/recipeActions';
+import ProfileHeader from '~/components/Header/ProfileHeader';
+import Text from '~/components/common/Text/Text';
+
+export default async function Recipe({
+  params,
+}: {
+  params: Promise<{ recipeId: string }>;
+}) {
+  const id = (await params).recipeId;
+  const data = await getRecipe(id);
+
+  return (
+    <>
+      <ProfileHeader />
+
+      <main>
+        <div>
+          <Text
+            as={'h2'}
+            weight={'bold'}
+            size={'heading'}
+            fontColor={'foreground'}
+          >
+            {data?.title}
+          </Text>
+
+          <div>{data?.viewCount}</div>
+        </div>
+
+        <div>
+          <Text
+            as={'h3'}
+            weight={'bold'}
+            size={'subheading'}
+            fontColor={'foreground'}
+          >
+            Tags
+          </Text>
+          <div>
+            {data?.tags.map((el, index) => (
+              <div key={index} className='flex gap-4'>
+                <div>{el}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <section>
+          <Text
+            as={'h3'}
+            weight={'bold'}
+            size={'subheading'}
+            fontColor={'foreground'}
+          >
+            Ingredients
+          </Text>
+          <div>
+            {(
+              data?.ingredients as {
+                name: string;
+                amount: string;
+                unit: string;
+              }[]
+            ).map((ingredient, index) => (
+              <div key={index} className='flex gap-4'>
+                <div>{ingredient.name}</div>
+                <div>{ingredient.amount}</div>
+                <div>{ingredient.unit}</div>
+              </div>
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <Text
+            as={'h3'}
+            weight={'bold'}
+            size={'subheading'}
+            fontColor={'foreground'}
+          >
+            Steps
+          </Text>
+          <div>
+            {data?.steps.map((step, index) => (
+              <div key={index}>
+                <p>{step}</p>
+              </div>
+            ))}
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
